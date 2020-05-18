8b0097612118837de14ff3e26a5b827450d2fcbb
Daniel <hafez.daniel@gmail.com>
2020-05-18 19:13:56 +0300

    created temporary mock data

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
 import flask
+from data import users, instruments_data
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
-def home():
+def homepage():
     return "<h1>Musical Instruments API</h1><p>This is a prototype API for managing musical instruments in venues.</p>"
 
 app.run()
+
+
+
 
---
+++ data.py
@@ -0,0 +1,57 @@
+
+users =  {
+        'first_name': 'daniel',
+        'last_name': 'hafez',
+        'id': 1
+    }, {
+        'first_name': 'john',
+        'last_name': 'guitar',
+        'id': 2
+    }, {
+        'first_name': 'michael',
+        'last_name': 'jax',
+        'id': 3
+    },{
+        'first_name': 'jimmy',
+        'last_name': 'hendrix',
+        'id': 4
+    }, {
+        'first_name': 'max',
+        'last_name': 'pezzali',
+        'id': 5
+    }, {
+        'first_name': 'eric',
+        'last_name': 'clapton',
+        'id': 6
+    }
+
+
+
+
+instruments_data = {
+    1: {
+        'id': 1,
+        'name': 'guitar',
+        'user': [users[0]],
+        'youtube': 'https://www.youtube.com/watch?v=w4a2ge9N31E'
+    },
+    2: {
+        'id': 2,
+        'name': 'piano',
+        'user': [users[1], users[2]],
+        'youtube': 'https://www.youtube.com/watch?v=w4a2ge9N31E'
+
+    },
+    3: {
+        'id': 3,
+        'name': 'bass',
+        'user': [users[2], users[3]],
+        'youtube': 'https://www.youtube.com/watch?v=w4a2ge9N31E'
+    },
+    4: {
+        'id': 4,
+        'name': 'violin',
+        'user': [users[5], users[4]],
+        'youtube': 'https://www.youtube.com/watch?v=w4a2ge9N31E'
+    }
+}
